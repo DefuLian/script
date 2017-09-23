9ca0c1f20a34d50b5b365fba5d3ed0eeb3aa9158
Defu Lian <dove.ustc@gmail.com>
2017-09-23 10:31:17 +1000

    implementing first model

--- dl/loc_pred/main.py
+++ dl/loc_pred/main.py
@@ -1,33 +1,54 @@
-
 import tensorflow as tf
 from keras.layers import Dot, Input, Lambda, multiply, dot
 import numpy as np
 from keras import backend as K
-sess = K.get_session()
-with sess.as_default():
-    x1 = K.constant(np.random.rand(1,2,3))
-    x2 = K.constant(np.random.rand(1,2,3))
-    y = dot([x1,x2], axes=2)
-    #y = Lambda(lambda v: K.sum(v,axis=-2))(y)
-    #print(x1.eval()); print( '\n'); print(x2.eval()); print('\n'); print( y.eval()); print(y.shape)
-
-    dataset = tf.contrib.data.Dataset.range(5)
-    dataset = dataset.shuffle(2)
-    #dataset = dataset.batch(3)
-    #dataset = dataset.map(lambda x: tf.fill([tf.cast(x, tf.int32)], x))
-    #dataset = dataset.padded_batch(4, padded_shapes=[None])
-
-    iterator = dataset.make_initializable_iterator()
-    sess.run(iterator.initializer)
-    next1 = iterator.get_next()
-    while True:
-        try:
-            v1= sess.run(next1)
-            print(v1)
-        except tf.errors.OutOfRangeError:
-            break
+tf.reset_default_graph()
+PAD = 0
+EOS = 1
+batch_size = 5
+seq_len_val = 20
+num_locations = 10
+embedding_size = 50
+hidden_units = embedding_size
+x = np.random.randint(1, num_locations, size=(batch_size, seq_len_val))
+#y = np.vstack((x.transpose(), [EOS] * batch_size))
+#y = y.transpose()[:, 1:]
+y = x.copy()
 
 
 
-#print y.eval()
-#print y.shape
+seq_len = tf.placeholder(tf.int32, name='sequence_length')
+input_seq = tf.placeholder(tf.int32, shape=(None, None), name='input_sequence')
+output_seq = tf.placeholder(tf.int32, shape=(None, None), name='output_sequence')
+embeddings = tf.Variable(tf.random_uniform([num_locations, embedding_size], -1.0, 1.0), dtype=tf.float32)
+
+input_embedding = tf.nn.embedding_lookup(embeddings, input_seq)
+output_embedding = tf.nn.embedding_lookup(embeddings, output_seq)
+
+rnn_cell = tf.contrib.rnn.LSTMCell(hidden_units)
+
+rnn_out, rnn_state = tf.nn.dynamic_rnn(
+    rnn_cell, input_embedding,
+    dtype=tf.float32, time_major=False,
+)
+
+W = tf.Variable(tf.zeros([hidden_units, num_locations]))
+W_t = tf.Variable(tf.zeros([num_locations, hidden_units]))
+b = tf.Variable(tf.zeros(num_locations))
+rnn_out_1 = tf.matmul(tf.reshape(rnn_out, (-1, hidden_units)), W)
+rnn_out_2 = tf.reshape(rnn_out_1, (-1, seq_len, num_locations))
+step_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=output_seq, logits=rnn_out_2)
+output_seq_reshape = tf.reshape(output_seq, [-1,1])
+rnn_out_reshape = tf.reshape(rnn_out, [-1, hidden_units])
+#step_loss = tf.nn.sampled_softmax_loss(W_t, b, output_seq_reshape , rnn_out_reshape, 500, num_locations)
+#step_loss_reshape = tf.reshape(step_loss, [-1, seq_len])
+#loss = tf.reduce_mean(tf.reduce_mean(step_loss_reshape, axis=[1]))
+loss = tf.reduce_mean(step_loss)
+train_op = tf.train.RMSPropOptimizer(learning_rate=0.1).minimize(loss)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for iter in range(20):
+        loss_val, _ = sess.run([loss, train_op],
+                               feed_dict={input_seq: x, output_seq: y, seq_len: seq_len_val})
+        print(loss_val)
+
