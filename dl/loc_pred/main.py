f701abe2bf717bc83fbf321880d2350aaeb0e321
Defu Lian <dove.ustc@gmail.com>
2017-09-25 17:46:02 +1000

    update

--- dl/loc_pred/main.py
+++ dl/loc_pred/main.py
@@ -1,17 +1,17 @@
 import tensorflow as tf
-from seq4model import basic_lm_loss
+from dl.loc_pred.seq4model import lm
 from keras.layers import Dot, Input, Lambda, multiply, dot
 import numpy as np
 from keras import backend as K
 tf.reset_default_graph()
 PAD = 0
 EOS = 1
-batch_size = 5
+batch_size = 6
 max_seq_len_val = 20
 num_locations = 10
 embedding_size = 50
 hidden_units = embedding_size
-x = np.random.randint(1, num_locations, size=(batch_size, max_seq_len_val))
+x = np.random.randint(1, num_locations, size=(batch_size, 5))
 #y = np.vstack((x.transpose(), [EOS] * batch_size))
 #y = y.transpose()[:, 1:]
 y = x.copy()
@@ -21,15 +21,16 @@ seq_mark = tf.sequence_mask(seq_len_val - 1, max_seq_len_val) ## leave the last 
 weight = tf.cast(seq_mark, tf.float32)
 
 weight_input = tf.placeholder(tf.int32, shape=(None,), name='example_weight')
-seq_length = tf.placeholder(tf.int32, shape=(None,), name='sequence_length')
-input_seq = tf.placeholder(tf.int32, shape=(None, None), name='input_sequence')
-output_seq = tf.placeholder(tf.int32, shape=(None, None), name='output_sequence')
-loss = basic_lm_loss(input_seq, output_seq, num_locations, embedding_size, weight, seq_length)
+train_input_seq = tf.placeholder(tf.int32, shape=(None, None), name='input_sequence')
+train_output_seq = tf.placeholder(tf.int32, shape=(None, None), name='output_sequence')
+test_input_seq = tf.placeholder(tf.int32, shape=(None, None), name='input_sequence')
+
+loss, test_pred = lm(train_input_seq, train_output_seq, test_input_seq, num_locations, embedding_size, weight_input)
 train_op = tf.train.RMSPropOptimizer(learning_rate=0.1).minimize(loss)
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for iter in range(20):
         loss_val, _ = sess.run([loss, train_op],
-                               feed_dict={input_seq: x, output_seq: y, max_seq_len: max_seq_len_val, seq_length:seq_len_val})
+                               feed_dict={train_input_seq: x, train_output_seq: y, weight_input:weight, test_input_seq: [x[:-1]]})
         print(loss_val)
 
--- dl/loc_pred/seq4model.py
+++ dl/loc_pred/seq4model.py
@@ -1,48 +1,114 @@
 import tensorflow as tf
 
-def basic_lm_loss(input_seq,
-                  output_seq,
-                  num_locations,
-                  embedding_size,
-                  weight,
-                  num_samples=-1):
+## train_input_seq, train_out_seq, test_intput_seq and train_weight is placeholder
 
+def lm(train_input_seq, train_out_seq, test_input_seq, num_locations, embedding_size, train_weight, num_samples=-1):
+    train_input_embedding = embed_seq(train_input_seq, num_locations, embedding_size)
+    test_input_embedding = embed_seq(test_input_seq, num_locations, embedding_size)
+    lm_loss(train_input_embedding, train_out_seq, test_input_embedding,
+            num_locations, embedding_size, train_weight, num_samples)
+
+def embed_seq(input_seq, num_locations, embedding_size):
     embeddings = tf.Variable(tf.random_uniform([num_locations, embedding_size], -1.0, 1.0), dtype=tf.float32)
     input_embedding = tf.nn.embedding_lookup(embeddings, input_seq)
-    rnn_cell = tf.contrib.rnn.LSTMCell(embedding_size)
+    return input_embedding
 
-    rnn_out, rnn_state = tf.nn.dynamic_rnn(
-        rnn_cell, input_embedding,
+def lm_loss(train_input_embedding,
+                  train_output_seq,
+                  test_input_embedding,
+                  num_locations,
+                  embedding_size,
+                  train_weight,
+                  num_samples=-1):
+
+    rnn_cell = tf.contrib.rnn.LSTMCell(embedding_size)
+    train_rnn_out, train_rnn_state = tf.nn.dynamic_rnn(
+        rnn_cell, train_input_embedding,
         dtype=tf.float32, time_major=False,
+    )
+
+    test_rnn_out, test_rnn_out = tf.nn.dynamic_rnn(
+        rnn_cell,test_input_embedding,
+        initial_state=train_rnn_state, dtype=tf.float32, time_major=False
     )
     W = tf.Variable(tf.zeros([embedding_size, num_locations]))
     b = tf.Variable(tf.zeros(num_locations))
-
-    max_seq_len = tf.shape(input_seq)[1]
+    train_output_seq_flat = tf.reshape(train_output_seq, [-1])
+    train_rnn_out_flat = tf.reshape(train_rnn_out, [-1, embedding_size])
     if num_samples>0:
         W_t = tf.transpose(W)
-        output_seq_flat = tf.reshape(output_seq, [-1])
-        rnn_out_flat = tf.reshape(rnn_out, [-1, embedding_size])
         step_loss = tf.nn.sampled_softmax_loss(weights=W_t, biases=b,
-                                               labels=output_seq_flat, inputs=rnn_out_flat,
+                                               labels=train_output_seq_flat, inputs=train_rnn_out_flat,
                                                num_sampled=num_samples, num_classes=num_locations)
     else:
-        rnn_out_1 = tf.matmul(tf.reshape(rnn_out, (-1, embedding_size)), W) + b
-        rnn_out_2 = tf.reshape(rnn_out_1, (-1, max_seq_len, num_locations))
-        step_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=output_seq, logits=rnn_out_2)
+        train_logit_flat = tf.matmul(train_rnn_out_flat, W) + b
+        step_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=train_output_seq_flat, logits=train_logit_flat)
 
-    step_loss_reshape = tf.reshape(step_loss, [-1, max_seq_len])
-    step_loss_reshape *= weight
-    loss = tf.reduce_mean(tf.reduce_sum(step_loss, axis=[1]))
+    step_loss *= tf.reshape(train_weight, [-1])
+    loss = tf.reduce_sum(step_loss)
+
+    test_rnn_out_flat = tf.reshape(test_rnn_out, [-1, embedding_size])
+    test_logit_flat = tf.matmul(test_rnn_out_flat, W) + b
+    _, test_pred_loc = tf.nn.top_k(test_logit_flat, k=10)
+
+    return loss, test_pred_loc
+
+def embed_list_tensor(input_seq, num_locations, embedding_size):
+    embeddings = tf.Variable(tf.random_uniform([num_locations, embedding_size], -1.0, 1.0), dtype=tf.float32)
+    input_embedding = [tf.nn.embedding_lookup(embeddings, e) for e in input_seq]
+    return input_embedding
 
 
-    #extract prediction part : here we only predict the last element
 
-    test_input_seq = tf.stack([input_seq[row, e-1] for (row, e) in enumerate(tf.unstack(seq_length))])
-    test_output_seq = tf.stack([input_seq[row, e-1] for (row, e) in enumerate(tf.unstack(seq_length))])
-    test_input_embedding = tf.nn.embedding_lookup(embeddings, test_input_seq)
-    test_rnn_out, _ = tf.nn.dynamic_rnn(rnn_cell, test_input_embedding, initial_state=rnn_state, dtype=tf.float32, time_major=False)
-    test_pred_score = tf.matmul(test_rnn_out, W) + b
-    test_pred_loc = tf.argmax(test_pred_score, 1)
-    acc = tf.reduce_mean(tf.case(tf.equal(test_output_seq, test_pred_loc), tf.float32))
-    return loss, acc
+# seq:  list of tensor, of size [batch], [time][batch][1]
+# labels: tensor, of size [batch]
+# seq_len: tensor of [batch], indicating length of each sequence
+
+
+def attention(query, states):
+    from keras.layers import Dense
+    att_size = states.get_shape()[2].value
+    batch_size = tf.shape(states)[0]
+    if query is not None:
+        y = Dense(att_size, activation='linear',use_bias=False)(query)
+    else:
+        y = tf.constant(tf.zeros(batch_size, att_size))
+    hidden = Dense(att_size, activation='linear',use_bias=False)(states)
+    v = tf.Variable(tf.zeros([att_size]))
+    s = tf.reduce_sum(v * tf.tanh(hidden + tf.reshape(y, [-1, 1, att_size])), [2])
+    s = tf.nn.softmax(s)
+    return s ### batch x time
+
+
+
+def classifier_seq(seq, labels, num_loc, embed_size, seq_len, num_samples=-1, k=1):
+    T = len(seq)
+    seq_embed = embed_list_tensor(seq, num_loc, embed_size)
+    fw_cell = tf.contrib.rnn.LSTMCell(embed_size)
+    bw_cell = tf.contrib.rnn.LSTMCell(embed_size)
+    seq_output, fw_state, bw_state = tf.nn.static_bidirectional_rnn(fw_cell, bw_cell, seq_embed, dtype=tf.float32, sequence_length=seq_len)
+    attention_state = [tf.reshape(e, [-1, 1, embed_size*2])for e in seq_output]
+    attention_state = tf.concat(attention_state, axis=1) # tensor of size batch x time x embed_size
+    weight = attention(None, attention_state)
+    weight_mask = tf.cast(tf.sequence_mask(seq_len, T), dtype=tf.float32)
+    weight *= weight_mask
+    weight /= tf.reduce_sum(weight, [-1], keep_dims=True)
+    attention_out = tf.reduce_sum(attention_state * tf.reshape(weight, [-1, T, 1]), [1])
+
+    W = tf.Variable(tf.zeros([embed_size, num_loc]))
+    b = tf.Variable(tf.zeros(num_loc))
+
+    if num_samples > 0:
+        W_t = tf.transpose(W)
+        step_loss = tf.nn.sampled_softmax_loss(weights=W_t, biases=b,
+                                               labels=labels, inputs=attention_out,
+                                               num_sampled=num_samples, num_classes=num_loc)
+    else:
+        step_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels,
+                                                                   logits=attention_out)
+    loss = tf.reduce_sum(step_loss)
+    logit = tf.matmul(attention_out, W) + b
+    _, pred_k_loc = tf.nn.top_k(logit, k)
+
+    return loss, pred_k_loc
+
